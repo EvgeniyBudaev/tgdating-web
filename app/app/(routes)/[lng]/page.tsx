bbc9fd98f07d9cb08621a5b4b9361bdcbbe003fd
Evgeniy Budaev <budaev.e@gmail.com>
2025-04-22 17:45:11 +0300

    feat: test navigator request

--- app/app/(routes)/[lng]/page.tsx
+++ app/app/(routes)/[lng]/page.tsx
@@ -1,26 +1,22 @@
-import { headers } from "next/headers";
-import { userAgentFromString } from "next/server";
-import { RootPage } from "@/app/pages/rootPage";
-import { ELanguage } from "@/app/shared/enums";
+"use client";
 
-export const dynamic = "force-dynamic";
+import { type FC, memo } from "react";
+import type { TRootPageProps } from "@/app/pages/rootPage/types";
+import "./RootPage.scss";
+import {useNavigator} from "@/app/shared/hooks";
 
-type TParams = Promise<{
-  lng: string;
-}>;
-
-export default async function RootRoute({ params }: { params: TParams }) {
-  const { lng } = await params;
-  const language = lng as ELanguage;
-
-  const headersList = await headers();
-  const userAgent = headersList.get("user-agent");
-  const { device } = userAgentFromString(userAgent || undefined);
-  const isMobile = device.type === "mobile";
-
+const RootPageComponent: FC<TRootPageProps> = (props) => {
+  const navigator = useNavigator({ lng: props.lng });
   return (
-    <main>
-      <RootPage isMobile={isMobile} lng={language} />
-    </main>
-  );
-}
+    <div>
+      <div>navigator: {JSON.stringify(navigator, null, 2)}</div>
+      <div>
+        <button onClick={navigator.getFromNavigator}>request location from navigator</button>
+      </div>
+    </div>
+  )
+};
+
+RootPageComponent.displayName = "RootPage";
+
+export const RootPage = memo(RootPageComponent);
